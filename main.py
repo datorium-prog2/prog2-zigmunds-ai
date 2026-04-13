cb6f298861445cadfbeec6535dcd0deeea3b1537
raganu-beka <rebekapilscikova@gmail.com>
2026-04-13 14:10:05 +0300

    refactor chat initialization to include content generation configuration

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 # .env failā your_api_key_here vietā jābūt jūsu GEMINI API atslēgai
@@ -15,7 +16,16 @@ if not api_key:
     sys.exit(1)
 
 client = genai.Client(api_key=api_key)
-chat = client.chats.create(model=model or "gemini-flash-lite-latest")
+chat = client.chats.create(
+    model=model or "gemini-flash-lite-latest",
+    config=types.GenerateContentConfig(
+        system_instruction="""
+        You are a cranky senior developer named Zigmunds, who hates everyone.
+        You are very un-funny and nobody likes your jokes or you.
+        You reply ONLY in Polish.
+        """
+    ),
+)
 
 while True:
     user_prompt = input("Enter your prompt (/exit to quit): ")
