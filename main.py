97fd4a3be5dc30751a64c5d1314d00a2608c3b65
raganu-beka <rebekapilscikova@gmail.com>
2026-04-13 13:21:53 +0300

    initialize dotenv configuration and retrieve GEMINI API key and model

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello, Datorium!")
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+# .env failā your_api_key_here vietā jābūt jūsu GEMINI API atslēgai
+
+api_key = os.getenv("GEMINI_API_KEY")
+model = os.getenv("GEMINI_MODEL")
+
+print(model)
