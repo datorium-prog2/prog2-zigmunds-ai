736e9b1c5c9002104b9b35604e64545f3371eed8
raganu-beka <rebekapilscikova@gmail.com>
2026-04-13 13:28:33 +0300

    implement content generation using GEMINI API client

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 # .env failā your_api_key_here vietā jābūt jūsu GEMINI API atslēgai
@@ -13,4 +14,9 @@ if not api_key:
     print("GEMINI_API_KEY is not set.")
     sys.exit(1)
 
-print(model)
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model=model or "gemini-flash-lite-latest", contents="Čau! Kā tev iet?"
+)
+print(response.text)
