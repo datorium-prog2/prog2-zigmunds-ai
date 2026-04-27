7cbf1bdc7801cebea64ba81cb9aea7bf8a47e22e
raganu-beka <rebekapilscikova@gmail.com>
2026-04-27 13:30:52 +0300

    feat: implement AgentSession class for managing user interactions and model responses

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from zigmunds_ai.tools import list_directory_files
-
 
 load_dotenv()
 # .env failā your_api_key_here vietā jābūt jūsu GEMINI API atslēgai
@@ -18,49 +16,7 @@ if not api_key:
     print("GEMINI_API_KEY is not set.")
     sys.exit(1)
 
-system_instruction = """
-        You are a cranky senior developer named Zigmunds, who hates everyone.
-        You are very un-funny and nobody likes your jokes or you.
-        You are heplful though, and do what is asked of you as a senior developer.
-        You reply ONLY Latvian.
-        """
 
-config = types.GenerateContentConfig(system_instruction=system_instruction, tools=TOOLS)
 
-client = genai.Client(api_key=api_key)
 
-history = []
-user_turn = True
 
-while True:
-
-    if user_turn:
-        user_prompt = input("Enter your prompt (/exit to quit): ")
-
-        if user_prompt.startswith("/exit"):
-            print("Exiting the program.")
-            sys.exit(0)
-
-        user_message = types.Content(
-            role="user", parts=[types.Part.from_text(text=user_prompt)]
-        )
-        history.append(user_message)
-
-    response = client.models.generate_content(
-        model=model or "gemini-flash-lite-latest", contents=history, config=config
-    )
-    history.append(response.candidates[0].content)  # type: ignore
-
-    if response.candidates[0].content.parts[0].function_call:  # type: ignore
-        function_call = response.candidates[0].content.parts[0].function_call  # type: ignore
-        function_result = TOOL_REGISTRY[function_call.name](**function_call.args)  # type: ignore
-        function_response = types.Part.from_function_response(
-            name=str(function_call.name),
-            response={"result": function_result},
-        )
-        history.append(function_response)
-        user_turn = False
-
-    else:
-        print(response.text)
-        user_turn = True
---
+++ zigmunds_ai/agent.py
@@ -0,0 +1,78 @@
+from google import genai
+from google.genai import types
+from registry import TOOL_REGISTRY, TOOLS
+
+
+class AgentSession:
+
+    SYSTEM_INSTRUCTIONS = """
+        You are a cranky senior developer named Zigmunds, who hates everyone.
+        You are very un-funny and nobody likes your jokes or you.
+        You are heplful though, and do what is asked of you as a senior developer.
+        You reply ONLY Latvian.
+        """
+
+    # __init__ klasē ir klases konstruktors
+    def __init__(self, api_key, model):
+        self.client = genai.Client(api_key=api_key)
+        self.config = types.GenerateContentConfig(
+            system_instruction=AgentSession.SYSTEM_INSTRUCTIONS, tools=TOOLS
+        )
+        self.model = model
+        self.running = False
+        self.history = []
+        self.user_turn = True
+
+    def run(self):
+        self.running = True
+
+        while self.running:
+            self._run_turn()
+
+        print("Exiting the program.")
+
+    def _run_turn(self):
+        if self.user_turn:
+            self._read_user_message()
+
+        response = self._get_model_response()
+        self.history.append(response)
+
+        function_call = self._get_function_call(response)
+        if function_call:
+            self._handle_function_call(function_call)
+            self.user_turn = False
+        else:
+            print(response.text)
+            self.user_turn = True
+
+    def _read_user_message(self):
+        user_prompt = input("Enter your message: ")
+
+        if user_prompt.startswith("/exit"):
+            self.running = False
+            return
+
+        user_message = types.Content(
+            role="user", parts=[types.Part.from_text(text=user_prompt)]
+        )
+        self.history.append(user_message)
+
+    def _get_model_response(self):
+        return self.client.models.generate_content(
+            model=self.model, contents=self.history, config=self.config
+        )
+
+    def _get_function_call(self, response):
+        if response.candidates[0].content.parts[0].function_call:  # type: ignore
+            return response.candidates[0].content.parts[0].function_call
+
+        return None
+
+    def _handle_function_call(self, function_call):
+        function_result = TOOL_REGISTRY[function_call.name](**function_call.args)  # type: ignore
+        function_response = types.Part.from_function_response(
+            name=str(function_call.name),
+            response={"result": function_result},
+        )
+        self.history.append(function_response)
